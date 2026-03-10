d09e53deeda0e8d23724c4adec0cb9629f45db3b
incharareddy <lakshmiincharam@gmail.com>
2026-03-10 23:00:53 +0530

    update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from app.core.database import Base, engine
+from app.models import job
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(router)
 
