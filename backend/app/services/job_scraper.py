bc884191d8f40f14242143f9222fbc03597baadc
chaithanyakrishnasn <chaithanyakrishnasn@gmail.com>
2026-03-12 22:45:19 +0530

    fix error

--- backend/app/services/job_scraper.py
+++ backend/app/services/job_scraper.py
@@ -1,26 +1,51 @@
-for job in data[1:6]:
+import requests
+from app.core.database import SessionLocal
+from app.models.job import Job
 
-    title = job.get("position")
-    company = job.get("company")
 
-    existing_job = db.query(Job).filter(
-        Job.title == title,
-        Job.company == company
-    ).first()
+def scrape_jobs():
 
-    if existing_job:
-        continue
+    url = "https://remoteok.com/api"
 
-    job_obj = Job(
-        title=title,
-        company=company,
-        url=job.get("url"),
-        status="new"
-    )
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-    db.add(job_obj)
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-    saved_jobs.append({
-        "title": job_obj.title,
-        "company": job_obj.company
-    })
+    db = SessionLocal()
+
+    saved_jobs = []
+
+    for job in data[1:6]:
+
+        title = job.get("position")
+        company = job.get("company")
+
+        existing_job = db.query(Job).filter(
+            Job.title == title,
+            Job.company == company
+        ).first()
+
+        if existing_job:
+            continue
+
+        job_obj = Job(
+            title=title,
+            company=company,
+            url=job.get("url"),
+            status="new"
+        )
+
+        db.add(job_obj)
+
+        saved_jobs.append({
+            "title": job_obj.title,
+            "company": job_obj.company
+        })
+
+    db.commit()
+    db.close()
+
+    return saved_jobs
