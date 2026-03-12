46ad85e994081c2d0864abf75fc1d23927b88985
chaithanyakrishnasn <chaithanyakrishnasn@gmail.com>
2026-03-12 22:40:17 +0530

    Prevent duplicate jobs from being saved

--- backend/app/services/job_scraper.py
+++ backend/app/services/job_scraper.py
@@ -1,37 +1,26 @@
-import requests
-from app.core.database import SessionLocal
-from app.models.job import Job
+for job in data[1:6]:
 
+    title = job.get("position")
+    company = job.get("company")
 
-def scrape_jobs():
-    url = "https://remoteok.com/api"
+    existing_job = db.query(Job).filter(
+        Job.title == title,
+        Job.company == company
+    ).first()
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+    if existing_job:
+        continue
 
-    response = requests.get(url, headers=headers)
-    data = response.json()
+    job_obj = Job(
+        title=title,
+        company=company,
+        url=job.get("url"),
+        status="new"
+    )
 
-    db = SessionLocal()
+    db.add(job_obj)
 
-    saved_jobs = []
-
-    for job in data[1:6]:
-        job_obj = Job(
-            title=job.get("position"),
-            company=job.get("company"),
-            url=job.get("url"),
-            status="new"
-        )
-
-        db.add(job_obj)
-        saved_jobs.append({
-            "title": job_obj.title,
-            "company": job_obj.company
-        })
-
-    db.commit()
-    db.close()
-
-    return saved_jobs
+    saved_jobs.append({
+        "title": job_obj.title,
+        "company": job_obj.company
+    })
