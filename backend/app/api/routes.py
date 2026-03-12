594d92f92538585af428e81c3d3e743516732ff9
chaithanyakrishnasn <chaithanyakrishnasn@gmail.com>
2026-03-12 22:32:25 +0530

    fix error

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from app.api.jobs import router as jobs_router
 
-router.include_router(jobs_router)
 router = APIRouter()
+
+router.include_router(jobs_router)
 
 @router.get("/health")
 def health_check():
--- backend/app/services/job_scraper.py
+++ backend/app/services/job_scraper.py
@@ -1,27 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
 
 
 def scrape_jobs():
-    url = "https://remoteok.com/remote-dev-jobs"
+    url = "https://remoteok.com/api"
 
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
 
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
+    data = response.json()
 
     jobs = []
 
-    for job in soup.select("tr.job")[:5]:
-        title = job.select_one("h2")
-        company = job.select_one("h3")
-
-        if title and company:
-            jobs.append({
-                "title": title.text.strip(),
-                "company": company.text.strip()
-            })
+    for job in data[1:6]:   # skip first metadata object
+        jobs.append({
+            "title": job.get("position"),
+            "company": job.get("company"),
+            "location": job.get("location"),
+        })
 
     return jobs
