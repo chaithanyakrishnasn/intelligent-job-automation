0c129ff6a4b3b2148d216aa7733ffa912c6a5cda
incharareddy <lakshmiincharam@gmail.com>
2026-03-12 22:37:26 +0530

    Add endpoint to fetch jobs from database

--- backend/app/api/jobs.py
+++ backend/app/api/jobs.py
@@ -1,8 +1,32 @@
 from fastapi import APIRouter
 from app.services.job_scraper import scrape_jobs
+from app.core.database import SessionLocal
+from app.models.job import Job
 
 router = APIRouter()
 
+
 @router.get("/jobs")
-def get_jobs():
+def run_scraper():
     return scrape_jobs()
+
+
+@router.get("/jobs/db")
+def get_jobs_from_db():
+    db = SessionLocal()
+
+    jobs = db.query(Job).all()
+
+    result = []
+
+    for job in jobs:
+        result.append({
+            "id": job.id,
+            "title": job.title,
+            "company": job.company,
+            "status": job.status
+        })
+
+    db.close()
+
+    return result
